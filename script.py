a7d20dde995a3b64103bd58fac565e767d0f265a
troygilmore3 <tgilmore3@gmail.com>
2020-04-25 14:11:49 -0400

    Initial Commit

---
+++ script.py
@@ -0,0 +1,17 @@
+import sys
+
+
+import requests
+
+
+print(sys.version)
+print(sys.executable)
+
+
+def greet(who_to_greet):
+    greeting = "Hello, {}".format(who_to_greet)
+    return greeting
+
+
+r = requests.get("https://google.com")
+print(r.status_code)
